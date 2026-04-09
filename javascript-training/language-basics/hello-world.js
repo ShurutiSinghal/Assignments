20df078b922cd218c160dc2888703420416ae09d
Shuruti Singhal <shurutisinghal23@gmail.com>
2026-04-09 23:01:54 +0530

    First code

---
+++ javascript-training/language-basics/hello-world.js
@@ -0,0 +1,12 @@
+console.log("Hello world!");
+
+//Print statement: Story of Hello World
+//1. Education and learning
+//2. Debugging and troubleshooting
+//3. Monitoring and logging
+//4. Testing and Validation
+//5. Communication and Collaboration
+
+console.error("This is error message");
+console.warn("This is warning message");
+console.info("This is informational message");
