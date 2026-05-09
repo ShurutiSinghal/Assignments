f64fc72403fa69e71c004fd86cc3ab0502266b4c
Shuruti Singhal <shurutisinghal23@gmail.com>
2026-05-09 19:16:56 +0530

    OOPS

---
+++ typescript-training/oops/variables/variables.ts
@@ -0,0 +1,19 @@
+//Regular variables versus read-only variables 
+
+//Regular variable
+
+class Employee {
+    readonly empId: number = 1234;
+    empName: string = "John Doe";
+
+    updateAndDisplayEmpDetails() {
+        this.empId = 5678; // Error: Cannot assign to 'empId' because it is a read-only property.
+        // it will execute as iternally it is converted to Javascript
+        
+        this.empName = "Jane Doe"; // Allowed: empName is not read-only
+        console.log(`Employee ID: ${this.empId}, Employee Name: ${this.empName}`);
+    }
+}
+
+let emp = new Employee();
+emp.updateAndDisplayEmpDetails();
